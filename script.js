cd4edb101e02984b55b27606095b825deaab66eb
Ryan Thomas <sawyer07@gmail.com>
2025-07-11 20:06:32 -0400

    initial upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+     // force mobile mode:
+
+     opt.displayMode = 'mobile';
+     update("curr.tab","quran");
+
+    var setvar = document.getElementById("side-content");
+
+    if(setvar) {
+         window.location.reload();
+     }
+
